f281072781e53990ba9dbfc60c832455a3bbe3c6
Hiroyuki Shindo <hshindo@outlook.com>
2016-03-06 22:29:09 +0900

    Update relu.cu

--- src/cuda/relu.cu
+++ src/cuda/relu.cu
@@ -1,3 +1,19 @@
+const int CUDA_NUM_THREADS = 512;
+
+// CUDA: number of blocks for threads.
+inline int GET_BLOCKS(const int N) { return (N + CUDA_NUM_THREADS - 1) / CUDA_NUM_THREADS; }
+
+// CUDA: grid stride looping
+#define CUDA_LOOP(i, n) \
+  for (int i = blockIdx.x * blockDim.x + threadIdx.x; i < (n); i += blockDim.x * gridDim.x)
+
+template <typename T>
+__global__ void relu_forward(T epsilon, T *out, int n) {
+	CUDA_LOOP(index, n) {
+		out[index] = out[index] > 0 ? out[index] : out[index] * epsilon;
+	}
+}
+
 template <typename T>
 __device__ void relu_forward(const int n, const T* in, T* out, T negative_slope) {
   CUDA_LOOP(index, n) {
