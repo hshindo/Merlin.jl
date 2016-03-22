f2502d30a2c4ca8e321164a72dcd526dbc809414
Hiroyuki Shindo <hshindo@outlook.com>
2016-03-23 03:05:15 +0900

    Fix doc

---
+++ docs/mathjaxhelper.js
@@ -0,0 +1,11 @@
+MathJax.Hub.Config({
+  "tex2jax": {
+    inlineMath: [ [ '$', '$' ] ],
+    displayMath: [ ['$$','$$'] ]
+  }
+});
+MathJax.Hub.Config({
+  config: ["MMLorHTML.js"],
+  jax: ["input/TeX", "output/HTML-CSS", "output/NativeMML"],
+  extensions: ["MathMenu.js", "MathZoom.js"]
+});
