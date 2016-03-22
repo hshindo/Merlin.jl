92b8f3c9426b50b9c96877ab0fa1c25c7f41ad76
Hiroyuki Shindo <hshindo@outlook.com>
2016-03-22 23:02:11 +0900

    Fix doc make

--- docs/make.jl
+++ docs/make.jl
@@ -1,5 +1,5 @@
 #push!(LOAD_PATH, joinpath(dirname(@__FILE__), "../"))
-push!(LOAD_PATH, "C:/Users/hshindo/Documents/GitHub")
+#push!(LOAD_PATH, "C:/Users/hshindo/Documents/GitHub")
 using Lapidary
 using Merlin
 
