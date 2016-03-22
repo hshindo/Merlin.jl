eab3d7c2dbd8a5c9a8914c47722cddca670f19e5
Hiroyuki Shindo <hshindo@outlook.com>
2016-03-22 23:56:01 +0900

    Fix doc make

--- docs/make.jl
+++ docs/make.jl
@@ -11,7 +11,7 @@ makedocs(
 custom_deps() = run(`pip install --user pygments mkdocs mkdocs-material`)
 
 deploydocs(
-  deps = custom_deps
+  deps = custom_deps,
   repo = "github.com/hshindo/Merlin.jl.git",
   julia  = "0.4"
 )
