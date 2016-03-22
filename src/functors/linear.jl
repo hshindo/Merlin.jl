4d830eb14003bf55b24747e51ca495cd3fee17ef
Hiroyuki Shindo <hshindo@outlook.com>
2016-03-23 02:13:53 +0900

    Fix doc

--- src/functors/linear.jl
+++ src/functors/linear.jl
@@ -4,7 +4,13 @@ using Base.LinAlg.BLAS
 """
 ## Linear
 Computes linear transformation a.k.a. affine transformation.
+
+\[
+  f(x) = wx
+\]
+
 \$\$f(x) = W^{\mathrm{T}}x + b\$\$
+
 where \(W\) is a weight matrix, \(b\) is a bias vector.
 
 ### Functions
