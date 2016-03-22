3feef3dddd9a711064775e2f00b9a699828bb612
Hiroyuki Shindo <hshindo@outlook.com>
2016-03-22 23:28:52 +0900

    FIx doc build

--- docs/make.jl
+++ docs/make.jl
@@ -3,9 +3,15 @@
 using Lapidary
 using Merlin
 
-makedocs()
+makedocs(
+  modules = [Merlin]
+  clean = false
+)
+
+custom_deps() = run(`pip install --user pygments mkdocs mkdocs-material`)
 
 deploydocs(
+  deps = custom_deps
   repo = "github.com/hshindo/Merlin.jl.git",
   julia  = "0.4"
 )
