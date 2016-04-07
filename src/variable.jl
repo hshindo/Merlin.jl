1723e9accd2fe32dfa276a648a9a34bccb04372e
Hiroyuki Shindo <hshindo@outlook.com>
2016-04-07 21:03:20 +0900

    Remove update for Vector{Functor}

--- src/variable.jl
+++ src/variable.jl
@@ -8,11 +8,6 @@ end
 
 Variable(value=nothing, grad=nothing) = Variable(value, grad, nothing, [], nothing)
 
-#function Variable(value=nothing; grad=nothing, f=nothing)
-#  Variable(value, grad, f, args, nothing, false)
-#end
-#Variable(value, f, args, gradient) = Variable(value, nothing, f, args, backward!)
-
 @compat function (f::Functor)(args::Vector{Variable})
   args[1].value == nothing && return Variable(nothing, nothing, f, args, nothing)
   xs = map(a -> a.value, args)
@@ -25,25 +20,6 @@ end
   y, backward = forward(f, arg.value)
   Variable(y, nothing, f, [arg], backward)
 end
-
-#=
-function Base.call{F<:Functor}(f::F, args::Vector{Variable})
-  v = Variable()
-  v.f = f
-  v.args = args
-  forward!(f, v)
-  v
-end
-Base.call{F<:Functor}(f::F, arg::Variable) = f([arg])
-Base.call{F<:Functor}(f::F, args::Variable...) = f([args...])
-
-function Base.call{T<:Vector}(fs::T, arg::Variable)
-  for f in fs
-    arg = call(f, arg)
-  end
-  arg
-end
-=#
 
 Base.getindex(v::Variable, key) = v.args[key]
 Base.setindex!(v::Variable, value, key) = v.args[key] = value
@@ -83,10 +59,4 @@ function topsort(var::Variable)
   end
   visit(var)
   sorted
-end
-
-function update!(opt::Optimizer, fs::Vector)
-  for f in fs
-    applicable(update!, opt, f) && update!(opt, f)
-  end
 end
