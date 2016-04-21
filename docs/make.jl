529f19aee43b83f76f41a5ebb3744cce0966b3c5
Hiroyuki Shindo <hshindo@outlook.com>
2016-04-21 19:05:18 +0900

    Fix typo

--- docs/make.jl
+++ docs/make.jl
@@ -6,7 +6,7 @@ makedocs(
 )
 
 deploydocs(
-  deps = deps   = Deps.pip("mkdocs", "python-markdown-math"),
+  deps = Deps.pip("mkdocs", "python-markdown-math"),
   repo = "github.com/hshindo/Merlin.jl.git",
   #julia  = "0.4"
 )
