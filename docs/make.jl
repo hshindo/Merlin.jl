80a65784941fc827b1c0250fdbb2ca4ad4710c62
Hiroyuki Shindo <hshindo@outlook.com>
2016-04-21 23:53:57 +0900

    Change theme of docs

--- docs/make.jl
+++ docs/make.jl
@@ -6,7 +6,7 @@ makedocs(
 )
 
 deploydocs(
-  deps = Deps.pip("mkdocs", "python-markdown-math"),
+  deps = Deps.pip("mkdocs", "mkdocs-material", "python-markdown-math", "pygments"),
   repo = "github.com/hshindo/Merlin.jl.git",
   #julia  = "0.4"
 )
