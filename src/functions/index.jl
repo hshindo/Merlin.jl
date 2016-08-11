413e05ca40f9b28cfa3c7d55534de4fe9e9dc02c
Hiroyuki Shindo <hshindo@outlook.com>
2016-08-12 01:44:48 +0900

    Remove type annotation of getindex

--- src/functions/index.jl
+++ src/functions/index.jl
@@ -1,7 +1,7 @@
 """
     getindex(x::Var, inds...)
 """
-function Base.getindex(x::Var, inds::Union{Range,Colon,Int}...)
+function Base.getindex(x::Var, inds...)
     y = x.data[inds...]
     df(gy) = hasgrad(x) && (x.grad[inds...] += gy) # TODO: more efficient in-place operation
     Var(y, [x], getindex, df)
