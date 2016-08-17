03cddd64a4be77e33c3ca2aa4e73969ab12dee2e
Hiroyuki Shindo <hshindo@outlook.com>
2016-08-18 04:01:57 +0900

    Update concat and reshape

--- src/functions/manipulation/reshape.jl
+++ src/functions/manipulation/reshape.jl
@@ -2,12 +2,9 @@ import Base.reshape
 
 """
     reshape(x::Var, dims::Int...)
-
-Reshape an array according to the given dimensions.
 """
-function reshape(x::Var, dims::Tuple)
+function reshape(x::Var, dims::Int...)
     y = reshape(x.data, dims)
-    df{T}(gy::UniArray{T}) = hasgrad(x) && BLAS.axpy!(T(1), reshape(gy, dims), x.grad)
+    df{T}(gy::UniArray{T}) = hasgrad(x) && BLAS.axpy!(T(1), gy, x.grad)
     Var(y, [x], reshape, df)
 end
-reshape(x::Var, dims::Int...) = reshape(x, dims)
