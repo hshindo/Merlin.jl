0435134e9df69c9efc05ce9f5122a97d2bb8292a
Hiroyuki Shindo <hshindo@outlook.com>
2016-08-31 23:02:39 +0900

    Add lrn

--- src/functions/lrn.jl
+++ src/functions/lrn.jl
@@ -1,3 +1,19 @@
 export lrn
+
+function lrn(x::Var)
+    y = lrn(x.data)
+    df(gy) = hasgrad(x) && ∇lrn!(x.data, x.grad, y.data, y.grad)
+    Var(y, [x], lrn, df)
+end
+
+function lrn(x::Array)
+    throw("Not implemented yet.")
+end
+
 lrn(x::CuArray) = JuCUDNN.lrn(x)
-∇lrn!(x::CuArray, gx::CuArray, y::CuArray, gy::CuArray) = JuCUDNN.∇lrn!(x, y, gy, gx)
+
+function ∇lrn!(x::Array, gx::Array, y::Array, gy::Array)
+    throw("Not implemented yet.")
+end
+
+∇lrn!(x::CuArray, gx, y, gy) = JuCUDNN.∇lrn!(x, y, gy, gx)
