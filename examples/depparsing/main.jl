15e384c1cd27ddabe616e012166565351ee2901b
Hiroyuki Shindo <hshindo@outlook.com>
2016-10-19 15:15:04 +0900

    Update depparsing

--- examples/depparsing/main.jl
+++ examples/depparsing/main.jl
@@ -1,6 +1,31 @@
 using Merlin
 using MLDatasets
 
+export CoNLL
+module CoNLL
+
+function read(f, path::String)
+    doc = []
+    sent = []
+    lines = open(readlines, path)
+    for line in lines
+        line = chomp(line)
+        if length(line) == 0
+            length(sent) > 0 && push!(doc, sent)
+            sent = []
+        else
+            items = Vector{String}(split(line,'\t'))
+            push!(sent, f(items))
+        end
+    end
+    length(sent) > 0 && push!(doc, sent)
+    T = typeof(doc[1][1])
+    Vector{Vector{T}}(doc)
+end
+read(path::String) = read(identity, path)
+
+end
+
 function main()
     traindata = CoNLL.read(x -> Token(x[2],x[5],), ".data/wsj_02-21.conll")
     testdata = CoNLL.read(".data/wsj_23.conll")
