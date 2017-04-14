d0d2fd65e3aac28ef11bb809f77bcacb536dd01d
Hiroyuki Shindo <hshindo@outlook.com>
2017-04-14 14:25:05 +0900

    Fix argmax

--- src/functions/argmax.jl
+++ src/functions/argmax.jl
@@ -2,7 +2,9 @@ export argmax
 
 function argmax(x::Array, dim::Int)
     _, index = findmax(x, dim)
-    ind2sub(size(x), vec(index))[dim]
+    y = ind2sub(size(x), vec(index))[dim]
+    dims = ntuple(i -> i==dim ? 1 : size(x,i), ndims(x))
+    reshape(y, dims)
 end
 
 """
