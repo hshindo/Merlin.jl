8ac601fc101c8d732ccc11a78614456d580dc551
Hiroyuki Shindo <hshindo@outlook.com>
2017-04-14 14:27:14 +0900

    Update postagging example

--- examples/postagging/main.jl
+++ examples/postagging/main.jl
@@ -4,9 +4,55 @@ using HDF5
 
 const h5file = "wordembeds_nyt100.h5"
 
-function main()
-    traindata, testdata, worddict, chardict, tagdict = setup_data()
+function setup_data()
+    words = h5read(h5file, "s")
+    worddict = Dict(words[i] => i for i=1:length(words))
+    chardict = Dict{Char,Int}()
+    tagdict = Dict{String,Int}()
 
+    traindoc = UD_English.traindata()
+    testdoc = UD_English.testdata()
+    #traindoc = CoNLL.read(".data/wsj_00-18.conll")
+    #testdoc = CoNLL.read(".data/wsj_22-24.conll")
+    info("# sentences of train doc: $(length(traindoc))")
+    info("# sentences of test doc: $(length(testdoc))")
+
+    train_x, train_y = setup_data(traindoc, worddict, chardict, tagdict)
+    test_x, test_y = setup_data(testdoc, worddict, chardict, tagdict)
+    info("# words: $(length(worddict))")
+    info("# chars: $(length(chardict))")
+    info("# tags: $(length(tagdict))")
+    train_x, train_y, test_x, test_y, worddict, chardict, tagdict
+end
+
+function setup_data(doc::Vector, worddict, chardict, tagdict)
+    data_x, data_y = Tuple{Var,Vector{Var}}[], Var[]
+    unkword = worddict["UNKNOWN"]
+    for sent in doc
+        w = Int[]
+        cs = Var[]
+        t = Int[]
+        for items in sent
+            word, tag = items[2], items[5]
+            word0 = replace(word, r"[0-9]", '0')
+            wordid = get(worddict, lowercase(word0), unkword)
+            chars = Vector{Char}(word0)
+            charids = map(c -> get!(chardict,c,length(chardict)+1), chars)
+            tagid = get!(tagdict, tag, length(tagdict)+1)
+            push!(w, wordid)
+            push!(cs, Var(reshape(charids,1,length(charids))))
+            push!(t, tagid)
+        end
+        w = reshape(w, 1, length(w))
+        t = reshape(t, 1, length(t))
+        push!(data_x, (Var(w),cs))
+        push!(data_y, Var(t))
+    end
+    data_x, data_y
+end
+
+function train()
+    train_x, train_y, test_x, test_y, worddict, chardict, tagdict = setup_data()
     wordembeds = h5read(h5file, "v")
     charembeds = rand(Float32, 10, 100)
     model = Model(wordembeds, charembeds, length(tagdict))
@@ -15,17 +61,16 @@ function main()
     for epoch = 1:10
         println("epoch: $epoch")
         opt.rate = 0.0075 / epoch
-        loss = fit(traindata, model, opt)
+        loss = fit(train_x, train_y, model, opt)
         println("loss: $loss")
 
-        ys = cat(1, map(x -> x[2].data, testdata)...)
-        zs = cat(1, map(x -> model(x[1]), testdata)...)
+        ys = cat(1, map(x -> vec(x.data), test_y)...)
+        zs = cat(1, map(x -> vec(model(x).data), test_x)...)
         acc = mean(i -> ys[i] == zs[i] ? 1.0 : 0.0, 1:length(ys))
         println("test acc.: $acc")
-        println("")
+        println()
     end
 end
 
-include("data.jl")
 include("model.jl")
-main()
+train()
--- examples/postagging/model.jl
+++ examples/postagging/model.jl
@@ -7,21 +7,21 @@ end
 function Model{T}(wordembeds::Matrix{T}, charembeds::Matrix{T}, ntags::Int)
     wordfun = Lookup(wordembeds)
 
-    x = Var()
+    x = Var([1,2,3])
     h = Lookup(charembeds)(x)
     h = window(h, (50,), pads=(20,), strides=(10,))
     h = Linear(T,50,50)(h)
     h = max(h, 2)
-    charfun = Graph(x, h)
+    charfun = Graph([x], [h])
 
-    w = Var()
-    c = Var()
+    w = Var(rand(T,100,3))
+    c = Var(rand(T,50,3))
     h = cat(1, w, c)
     h = window(h, (750,), pads=(300,), strides=(150,))
     h = Linear(T,750,300)(h)
     h = relu(h)
     h = Linear(T,300,ntags)(h)
-    sentfun = Graph((w,c), h)
+    sentfun = Graph([w,c], [h])
 
     Model(wordfun, charfun, sentfun)
 end
@@ -32,5 +32,5 @@ function (m::Model)(input::Tuple{Var,Vector{Var}}, y=nothing)
     cvecs = map(m.charfun, cs)
     cmat = cat(2, cvecs)
     x = m.sentfun(wmat, cmat)
-    y == nothing ? argmax(x.data,1) : crossentropy(y,x)
+    y == nothing ? argmax(x,1) : crossentropy(y,x)
 end
