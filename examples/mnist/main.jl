404ff2de82ede0ae358adfc779a3eb493ba5d6b4
Hiroyuki Shindo <hshindo@outlook.com>
2017-04-14 14:27:01 +0900

    Update mnist

--- examples/mnist/main.jl
+++ examples/mnist/main.jl
@@ -1,22 +1,20 @@
 using Merlin
 using MLDatasets
 
-const save_model = false
-
 function setup_data(x::Array, y::Vector)
     x = reshape(x, size(x,1)*size(x,2), size(x,3))
     x = Matrix{Float32}(x)
     batchsize = 100
     xs = [Var(x[:,i:i+batchsize-1]) for i=1:batchsize:size(x,2)]
     y += 1 # Change label set: 0..9 -> 1..10
-    ys = [Var(y[i:i+batchsize-1]) for i=1:batchsize:length(y)]
+    y = reshape(y, 1, length(y))
+    ys = [Var(y[:,i:i+batchsize-1]) for i=1:batchsize:length(y)]
     xs, ys
 end
 
-function main(nepochs::Int)
+function train(model, nepochs::Int)
     train_x, train_y = setup_data(MNIST.traindata()...)
     test_x, test_y = setup_data(MNIST.testdata()...)
-    model = Model(Float32, 1000)
 
     opt = SGD(0.0001, momentum=0.99, nesterov=true)
     for epoch = 1:nepochs
@@ -24,17 +22,16 @@ function main(nepochs::Int)
         loss = fit(train_x, train_y, model, opt)
         println("loss: $loss")
 
-        # predict
-        ys = cat(1, map(data, test_y)...)
-        zs = cat(1, map(model, test_x)...)
+        ys = cat(1, map(x -> vec(x.data), test_y)...)
+        zs = cat(1, map(x -> vec(model(x).data), test_x)...)
         acc = mean(i -> ys[i] == zs[i] ? 1.0 : 0.0, 1:length(ys))
         println("test accuracy: $acc")
         println()
-        if save_model && epoch == nepochs
-            Merlin.save("mnist_epoch$(nepochs).h5", "g"=>model.g)
-        end
     end
+    Merlin.save("mnist_epoch$(nepochs).h5", "model"=>model)
 end
 
 include("model.jl")
-main(10)
+model = Model(Float32, 1000)
+#model = Merlin.load(joinpath(dirname(@__FILE__),"mnist_epoch10.h5"), "model")
+train(model, 10)
--- examples/mnist/model.jl
+++ examples/mnist/model.jl
@@ -3,18 +3,18 @@ type Model
 end
 
 function Model{T}(::Type{T}, hsize::Int)
-    x = Var()
-    y = Var()
+    x = Var(rand(T,784,10))
+    y = Var(rand(1:1,1,10))
     h = Linear(T,784,hsize)(x)
     h = relu(h)
     h = Linear(T,hsize,hsize)(h)
     h = relu(h)
     h = Linear(T,hsize,10)(h)
-    g = Graph(x, h)
+    g = Graph([x], [h])
     Model(g)
 end
 
 function (m::Model)(x::Var, y=nothing)
     z = m.g(x)
-    y == nothing ? argmax(z.data,1) : crossentropy(y,z)
+    y == nothing ? argmax(z,1) : crossentropy(y,z)
 end
