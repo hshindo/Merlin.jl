cbf38ebd5adee2cdbb0a0519660ba5651e43d707
Hiroyuki Shindo <hshindo@outlook.com>
2017-06-07 22:51:12 +0900

    Update

--- examples/postagging/model.jl
+++ examples/postagging/model.jl
@@ -1,17 +1,20 @@
-function create_model(chardim::Int, worddim::Int, numtags::Int)
+function create_model(data_x, data_y)
     T = Float32
-
-    w = Lookup()(x)
-
-    h = Lookup()(x)
-    h = Conv1D(T,50,20,10))(h)
-    c = max(h, 2)
-    compile(c, x)
-
-    h = cat(1, w, c)
-    h = Conv1D(T,750,300,150)(h)
-    h = relu(h)
-    h = Linear(T,300,numtags)(h)
-
-    compile(h, x, c)
+    nn = For(1:length(data_x)) do i
+        x, y = data_x[i], data_y[i]
+        wc = For(1:length(x)) do k
+            w = Lookup(T,10000,100)(x)
+            c = Lookup(T,10000,100)(x)
+            h = Conv1D(T,50,20,10))(h)
+            c = max(h, 2)
+            cat(1, w, c)
+        end
+        h = cat(1, w, c)
+        h = Conv1D(T,750,300,150)(h)
+        h = relu(h)
+        h = Linear(T,300,numtags)(h)
+        z = softmax(h)
+        crossentropy(y, z)
+    end
+    compile(nn)
 end
