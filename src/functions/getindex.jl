23ef3fcf1e74c270d543436c331b571246a25213
Hiroyuki Shindo <hshindo@users.noreply.github.com>
2017-11-25 16:42:25 +0900

    Update getindex

--- src/functions/getindex.jl
+++ src/functions/getindex.jl
@@ -12,12 +12,19 @@ Note that `y = x[i]` throws an error since `y` is not a vector but a scholar.
 Instead, use `y = x[i:i]`.
 """
 function getindex(x::Var, inds::Tuple)
+    @assert ndims(x) == length(inds)
+    subxs = unsafe_split(x.data, x.batchdims)
+    if !all(subx -> validindex(size(subx,ndims(subx)),inds[end]), subxs)
+        issorted(x.batchdims,rev=true) || throw("x.batchdims must be sorted in descending order: $(x.batchdims).")
+    end
+
     ys = []
-    for s in unsafe_split(x.data,x.batchdims)
+    for s in subxs
+        validindex(size(s,ndims(x)), inds[end]) || break
         push!(ys, s[inds...])
     end
     y = cat(ndims(x), ys...)
-    batchdims = map(x -> size(x,ndims(x)), ys)
+    batchdims = map(y -> size(y,ndims(x)), ys)
     Var(y, batchdims, getindex, (x,inds))
 end
 getindex(x::Var, inds::Union{Int,Range,Colon}...) = getindex(x, inds)
@@ -35,3 +42,7 @@ function addgrad!(y::Var, ::typeof(getindex), x::Var, inds::Tuple)
         end
     end
 end
+
+validindex(dim::Int, ind::Int) = dim >= ind
+validindex(dim::Int, ind::Range) = dim >= last(ind)
+validindex(dim::Int, ind::Colon) = true
