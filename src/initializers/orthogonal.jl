6703548d14ce7500851f9eaeb9c819c895068da1
Hiroyuki Shindo <hshindo@outlook.com>
2017-11-29 15:51:38 +0900

    Fix orthogonal to use gain

--- src/initializers/orthogonal.jl
+++ src/initializers/orthogonal.jl
@@ -1,17 +1,19 @@
 export Orthogonal
 
 doc"""
-    Orthogonal()
+    Orthogonal([gain=1.0])
 
-## References
+# References
 * Saxe et al., [Exact solutions to the nonlinear dynamics of learning in deep linear neural networks](http://arxiv.org/abs/1312.6120)
 """
 struct Orthogonal
+    gain::Float64
 end
+Orthogonal() = Orthogonal(1.0)
 
-function (::Orthogonal)(::Type{T}, dim1::Int, dim2::Int) where T
+function (o::Orthogonal)(::Type{T}, dim1::Int, dim2::Int) where T
     a = randn(T, dim1, dim2)
     u, _, v = svd(a)
     q = size(u) == (dim1,dim2) ? u : v
-    q * T(scale)
+    q * T(o.gain)
 end
