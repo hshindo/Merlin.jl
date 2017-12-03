a58b40ff21081e52d9ae7e7aa3328ae4ff8bb624
Hiroyuki Shindo <hshindo@users.noreply.github.com>
2017-12-03 10:52:50 +0900

    Merge resize with reshape

--- src/functions/reshape.jl
+++ src/functions/reshape.jl
@@ -1,20 +1,26 @@
 import Base.reshape
 
 doc"""
-    reshape(x, dims::Int...)
-    reshape(x, dims::Tuple)
+    reshape(x, dims::Tuple, batchdims::Vector{Int})
+
+# Example
+```julia
+T = Float32
+x = Var(rand(T,10,5))
+y = reshape(x, (2,5), [2,3])
+```
 """
-function reshape(x::Var, dims::Tuple)
-    throw("Not implemented yet.")
-    Var(reshape(x.data,dims), x.batchdims, reshape, (x,dims))
+function reshape(x::Var, dims::Tuple, batchdims::Vector{Int})
+    y = reshape(x.data, dims)
+    sum(batchdims) == size(y,ndims(y)) || throw("Invalid batchdims: $(size(y)) and $batchdims")
+    Var(y, batchdims, reshape, (x,))
 end
-reshape(x::Var, dims::Int...) = reshape(x, dims)
 
-reshape(x::Node, dims::Tuple; name="") = Node(reshape, (x,dims), name)
-reshape(x::Node, dims::Int...) = reshape(x, dims)
+reshape(x::Node, dims::Tuple, batchdims::Vector{Int}; name="") = Node(reshape, (x,dims), name)
 
-function addgrad!(y::Var, ::typeof(reshape), x::Var, dims::Tuple)
-    isvoid(x.grad) && return
-    T = eltype(x.data)
-    BLAS.axpy!(T(1), y.grad, x.grad)
+function addgrad!(y::Var, ::typeof(reshape), x::Var)
+    if !isvoid(x.grad)
+        T = eltype(x)
+        BLAS.axpy!(T(1), y.grad, x.grad)
+    end
 end
