5464bde38a16d865323361aacade8adae6487624
Hiroyuki Shindo <hshindo@users.noreply.github.com>
2017-12-25 07:22:17 +0900

    [WIP] CUDA

--- src/cuda/execution.jl
+++ src/cuda/execution.jl
@@ -3,6 +3,9 @@ box(x::Int) = Cint(x)
 box{N}(t::NTuple{N,Int}) = map(Cint, t)
 box(x::Vector{Int}) = ntuple(i -> Cint(x[i]), length(x))
 
+macro cuda
+end
+
 function (f::CuFunction)(args...;
     dx=1, dy=1, dz=1, bx=128, by=1, bz=1, sharedmem=0, stream=C_NULL)
     argptrs = Ptr{Void}[pointer_from_objref(box(a)) for a in args]
