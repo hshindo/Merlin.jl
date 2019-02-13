ff7027f5d3b8f1c5452ff492a392ab0ae218bd4c
Hiroyuki Shindo <hshindo@users.noreply.github.com>
2019-02-14 03:07:36 +0900

    Update crf

--- src/functions/rnncrf.jl
+++ src/functions/rnncrf.jl
@@ -2,28 +2,41 @@ export RNNCRF
 
 mutable struct RNNCRF <: Functor
     beta::Var
-    cnn
-    mu::Var
+    mu1::Var
+    mu2::Var
+    niters::Int
 end
 
-function RNNCRF(::Type{T}, insize::Int, temp) where T
-    beta = Var(fill(T(-1/temp), insize))
-    cnn = Conv1d(T, 3, insize, insize, padding=1)
-    mu = fill(T(1/insize), insize, insize)
-    RNNCRF(cnn, parameter(mu))
+function RNNCRF(::Type{T}, insize::Int) where T
+    beta = parameter(fill(T(1), insize, 1))
+    mu1 = parameter(fill(T(1/insize),insize,insize))
+    mu2 = parameter(fill(T(1/insize),insize,insize))
+    RNNCRF(beta, mu1, mu2, 5)
 end
 
-function (nn::RNNCRF)(u::Var, x::Var, dims, niters::Int)
-    
-
-    q = nn.beta .* u
-    q = softmax(q)
-    for i = 1:niters
-        # q = concat(1, q, f)
-        q = nn.cnn(q, dims)
-        q = nn.mu * q
+function (nn::RNNCRF)(u::Var, dims)
+    q = softmax(u)
+    bos = zero(q, size(q,1), 1)
+    for i = 1:nn.niters
+        # q = nn.beta .* q
+        q1 = nn.mu1 * q
+        q2 = nn.mu2 * q
+        q1s = Var[]
+        q2s = Var[]
+        offs = 0
+        for d in dims
+            if d == 1
+                push!(q1s, bos)
+                push!(q2s, bos)
+            else
+                push!(q1s, bos, q1[:,offs+1:offs+d-1])
+                push!(q2s, q2[:,offs+2:offs+d], bos)
+            end
+            offs += d
+        end
+        q = concat(2, q1s...) + concat(2, q2s...)
         q += u
-        i == niters || (q = softmax(q))
+        i == nn.niters || (q = softmax(q))
     end
     q
 end
