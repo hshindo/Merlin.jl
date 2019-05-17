2460e7fd5173903db7154fc383165e7af336c681
Hiroyuki Shindo <hshindo@outlook.com>
2019-05-17 13:51:35 +0900

    Fix clipping

--- src/functions/clip.jl
+++ src/functions/clip.jl
@@ -20,9 +20,25 @@ function clipnorm!(x::CuArray{T}, threshold) where T
     x .*= z
 end
 
-function clipvalue!(x::Array{T}, value) where T
+function clipvalue!(x::Array{T}, value::T) where T
     value = T(value)
     @inbounds for i = 1:length(x)
         x[i] = min(max(x[i],-value), value)
+    end
+end
+
+@generated function clipvalue!(x::CuArray{T}, value::T) where T
+    Ct = cstring(T)
+    k = Kernel("""
+    __global__ void clip($Ct *x, $Ct value, int n) {
+        int idx = blockIdx.x * blockDim.x + threadIdx.x;
+        if (idx >= n) return;
+        x[idx] = min(max(x[idx],-value), value);
+    }""")
+    quote
+        @assert value > T(0)
+        gdims, bdims = cudims(length(x))
+        $k(gdims, bdims, pointer(x), value, length(x)) # TODO: handle with int case
+        x
     end
 end
